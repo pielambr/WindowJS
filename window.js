50082170a1d96af7a45f1a90026eb3e4b2c86062
Pieterjan <me@pielambr.be>
2015-06-14 20:46:06 +0200

    [enhancement] using semantic html tags

--- window.js
+++ window.js
@@ -8,29 +8,27 @@ function WindowJS (options) {
         // Get a reference to body DOM element
         this.body = document.getElementsByTagName("body")[0];
         // Create outer window div
-        this._window = document.createElement("div");
-        this._window.className = "windowjs-window";
+        this._window = this.createElement("article", "windowjs-window");
         this._window.style.position = "absolute";
+        if("title" in options || "close_button" in options) {
+            this._header = this.createElement("header", "windowjs-header");
+            this.addToWindow(this._header);
+        }
         // Add title div
         if("title" in options) {
-            this._title = document.createElement("div");
-            this._title.className = "windowjs-title";
-            this._title.innerHTML = options.title;
-            this.addToWindow(this._title);
+            this._title = this.createElement("div", "windowjs-title", options.title);
+            this.addToHeader(this._title);
         }
         // Add a close button and make closeable
         if("close_button" in options) {
-            this._close = document.createElement("div");
-            this._close.className = "windowjs-close";
-            this._close.innerHTML = options.close_button;
+            this._close = this.createElement("div", "windowjs-close", options.close_button);
             this._close.onclick = function() {
                 that.close();
             }
-            this.addToWindow(this._close);
+            this.addToHeader(this._close);
         }
         // Make draggable if necessary
-        if("draggable" in options
-            && options.draggable === true) {
+        if("draggable" in options && options.draggable === true) {
             this.draggable = options.draggable;
             this.attachDrag();
         }
@@ -43,9 +41,7 @@ function WindowJS (options) {
         this._window.id = this._id;
         // Add some main content to the window
         if("content" in options){
-            this._body = document.createElement("div");
-            this._body.className = "windowjs-body";
-            this._body.innerHTML = options.content;
+            this._body = this.createElement("main", "windowjs-body", options.content);
             this.addToWindow(this._body);
         }
         // Add a parent div for our window
@@ -60,9 +56,24 @@ function WindowJS (options) {
     }
 }
 
+WindowJS.prototype.createElement = function(tag, className, html) {
+    var el = document.createElement(tag);
+    if(className) {
+        el.className = className;
+    }
+    if(html) {
+        el.innerHTML = html;
+    }
+    return el;
+};
+
 // Add a DOM element to our window
 WindowJS.prototype.addToWindow = function(el) {
     this._window.appendChild(el);
+};
+
+WindowJS.prototype.addToHeader = function(el) {
+    this._header.appendChild(el);
 };
 
 // Remove the window from the DOM
